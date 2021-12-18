07992f5e34da7e01b01c35baf3c4895e5b74368d
Goncalo Pereira <Goncalo.Pereira.22205@formandos.cinel.pt>
2021-12-18 15:30:29 +0000

    Add project files.

---
+++ Reviews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Reviews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
